932689c224d5528d866c5ed41d4b77422ab9bcb2
arnesh2212 <66691657+arnesh2212@users.noreply.github.com>
2021-05-14 20:50:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,34 @@ import streamlit as st
 import tensorflow as tf 
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import io
+import json
 
-st.title("Marvel Text Generator")
+st.title("Neural Marvel Text Generator")
 st.write("Type the starting text (marvel related preferebly) and then press enter ( eg- iron man killed thanos ....)")
 model = tf.keras.models.load_model("model.h5")
 
+#tokenizer = Tokenizer()
+
+#data = open('data.txt').read()
+
+#corpus = data.lower().split("\n")
+
+#tokenizer.fit_on_texts(corpus)
+#total_words = len(tokenizer.word_index) + 1
 
 
 
 
-import pickle
+#tokenizer_json = tokenizer.to_json()
+#with io.open('tokenizer.json', 'w', encoding='utf-8') as f:
+#    f.write(json.dumps(tokenizer_json, ensure_ascii=False))
 
-#with open('tokenizer.pickle', 'wb') as handle:
-#    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+with open('tokenizer.json') as f:
+    data = json.load(f)
+    tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(data)
 
-# loading
 
-with open('tokenizer.pickle', 'rb') as handle:
-    tokenizer = pickle.load(handle)
-total_words = len(tokenizer.word_index) + 1
 
 # pad sequences 
 max_sequence_len = 23
