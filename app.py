405a95156ed927c3a064c42b6594c386e413a6e3
arnesh2212 <66691657+arnesh2212@users.noreply.github.com>
2021-05-14 19:30:55 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,27 +4,27 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 st.title("Marvel Text Generator")
-st.write("Type the starting text and then press enter ( eg- iron man :)")
+st.write("Type the starting text (marvel related preferebly) and then press enter ( eg- iron man killed thanos ....)")
 model = tf.keras.models.load_model("model.h5")
 
-tokenizer = Tokenizer()
 
-data = open('data.txt').read()
 
-corpus = data.lower().split("\n")
 
-tokenizer.fit_on_texts(corpus)
+
+import pickle
+
+#with open('tokenizer.pickle', 'wb') as handle:
+#    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# loading
+
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
 total_words = len(tokenizer.word_index) + 1
 
-input_sequences = []
-for line in corpus:
-	token_list = tokenizer.texts_to_sequences([line])[0]
-	for i in range(1, len(token_list)):
-		n_gram_sequence = token_list[:i+1]
-		input_sequences.append(n_gram_sequence)
-
 # pad sequences 
-max_sequence_len = max([len(x) for x in input_sequences])
+max_sequence_len = 23
+
 user_input = st.text_input("Enter Seed Text")
 
 seed_text = user_input
